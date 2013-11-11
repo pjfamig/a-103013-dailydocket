af9b10cd0ff6b286bb4e429346740cee424aa023
Paul Famiglietti <p0seid0n_@hotmail.com>
2013-11-11 12:53:34 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+A103013Dailydocket::Application.config.secret_key_base = '4c55457193d1a2348a2a2b8e64900b728564f758bf70a03ee64f05f93c49033b6f42886b9ab0bc5d3cd1133ad7fbe7ccb17d5c4eae21aa2d21a52ea5df1161a4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+A103013Dailydocket::Application.config.session_store :cookie_store, key: '_a-103013-postgresql_session'
