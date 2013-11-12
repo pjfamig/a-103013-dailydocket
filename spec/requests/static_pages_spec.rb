43763281ac67562c575ece5c0aa062d9f582e8e6
Paul Famiglietti <p0seid0n_@hotmail.com>
2013-11-11 21:14:37 -0800

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  describe "Home page" do
+
+    it "should have the content 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Home')
+    end
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("DailyDocket | Home")
+    end
+  end
+  
+  describe "About page" do
+
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("DailyDocket | About Us")
+    end
+  end
+end
